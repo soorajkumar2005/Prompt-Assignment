9c1fa396ed01879296f321f06b9b2f504edf4b38
soorajkumar2005 <soorajku2021@gmail.com>
2023-04-08 11:19:24 +0500

    second

---
+++ app.js
@@ -0,0 +1,33 @@
+var fullName = prompt("Name");
+var fatherName = prompt("Father's Name");
+var email = prompt("Email");
+var password = prompt("Password");
+var cnicNumber = prompt("CNIC Number");
+var mobileNumber = prompt("Mobile Number");
+var courseName = prompt("Course Name");
+var qualification = prompt("Qualification");
+var gender = prompt("Gender");
+var question = prompt("Do you have Laptop for Classes: YES/NO");
+
+console.log("Name :- " + fullName);
+console.log("Father's Name :- " + fatherName);
+console.log("Email :- " + email);
+console.log("Password :- " + password);
+console.log("CNIC Number :- " + cnicNumber);
+console.log("Mobile Number :- " + mobileNumber);
+console.log("Course Name :- " + courseName);
+console.log("Qualification :- " + qualification);
+console.log("Gender :- " + gender);
+console.log("Do you have Laptop for Classes :- " + question);
+
+document.write("Name :- " + fullName +"<br>" + "<br>");
+document.write("Father's Name :- " + fatherName +"<br>" + "<br>");
+document.write("Email :- " + email +"<br>" + "<br>");
+document.write("Password :- " + password +"<br>" + "<br>");
+document.write("CNIC Number :- " + cnicNumber +"<br>" + "<br>");
+document.write("Mobile Number :- " + mobileNumber +"<br>" + "<br>");
+document.write("Course Name :- " + courseName +"<br>" + "<br>");
+document.write("Qualification :- " + qualification +"<br>" + "<br>");
+document.write("Gender :- " + gender +"<br>" + "<br>");
+document.write("Do you have Laptop for Classes :- " + question +"<br>" + "<br>");
+
